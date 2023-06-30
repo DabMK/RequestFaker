3e6e7f993ad99c681468b4b9a256eede11e1b905
DabMK <64862398+DabMK@users.noreply.github.com>
2023-06-30 13:55:35 +0200

    Added files

---
+++ Program.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Security.Authentication;
+
+#pragma warning disable CS0618
+#pragma warning disable SYSLIB0039
+namespace RequestFaker
+{
+    internal class Program
+    {
+        private static string? URI = null, method = null, body = null, customVersion = null, protocol = "1.1";
+        private static string[]? proxy = null;
+        private static bool autoRedirects = true, ssl = false;
+        private static Dictionary<string, string>? headers = null, cookies = null;
+        private static Request.ProtocolVersion? version = null;
+        private static HttpVersionPolicy? policy = null;
+        private static SslProtocols? SSLprotocol = null;
+        private static DecompressionMethods? decompression = null;
+        public static string? Protocol { set { protocol = value; } }
+
+        private static void Main(string[] args)
+        {
+            bool ok = true;
+            while (true)
+            {
+                Initialize();
+                Console.Clear();
+                Console.WriteLine("------------------------");
+                Console.WriteLine("RequestFaker");
+                Console.WriteLine("------------------------\n");
+
+                Console.Write("\nURI: ");
+                URI = Console.ReadLine();
+
+                Console.Write("\nMethod: ");
+                method = Console.ReadLine();
+
+                Console.Write("\nHeaders Number (Without cookies): ");
+                if (int.TryParse(Console.ReadLine(), out int hNum) && hNum >= 1)
+                {
+                    headers = new(hNum);
+                    for (int i = 0; i < hNum; i++)
+                    {
+                        Console.Write($"- Header {i + 1} Key: ");
+                        string? key = Console.ReadLine();
+                        if (string.IsNullOrWhiteSpace(key)) { ok = false; break; }
+                        Console.Write($"- Header {i + 1} Value: ");
+                        string? value = Console.ReadLine();
+                        if (string.IsNullOrWhiteSpace(value)) { ok = false; break; }
+                        headers.Add(key, value);
+                    }
+                    if (!ok) { continue; }
+                }
+
+                Console.Write("\nBody: ");
+                body = Console.ReadLine();
+
+                Console.Write("\nCookies Number: ");
+                if (int.TryParse(Console.ReadLine(), out int cNum) && cNum >= 1)
+                {
+                    cookies = new(cNum);
+                    for (int i = 0; i < cNum; i++)
+                    {
+                        Console.Write($"- Cookie {i + 1} Name: ");
+                        string? key = Console.ReadLine();
+                        if (string.IsNullOrWhiteSpace(key)) { ok = false; break; }
+                        Console.Write($"- Cookie {i + 1} Value: ");
+                        string? value = Console.ReadLine();
+                        if (string.IsNullOrWhiteSpace(value)) { ok = false; break; }
+                        cookies.Add(key, value);
+                    }
+                    if (!ok) { continue; }
+                }
+
+                Console.Write("\nVersion: ");
+                string? sVersion = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(sVersion) && !sVersion.StartsWith("0"))
+                {
+                    if (sVersion == "1" || sVersion == "1.0" || sVersion == "10" || sVersion == "v10" || sVersion == "v1.0" || sVersion == "v1")
+                    {
+                        version = Request.ProtocolVersion.v10;
+                    }
+                    else if (sVersion == "1.1" || sVersion == "11" || sVersion == "v11" || sVersion == "v1.1")
+                    {
+                        version = Request.ProtocolVersion.v11;
+                    }
+                    else if (sVersion == "2" || sVersion == "2.0" || sVersion == "20" || sVersion == "v20" || sVersion == "v2.0" || sVersion == "v2")
+                    {
+                        version = Request.ProtocolVersion.v20;
+                    }
+                    else if (sVersion == "3" || sVersion == "3.0" || sVersion == "30" || sVersion == "v30" || sVersion == "v3.0" || sVersion == "v3")
+                    {
+                        version = Request.ProtocolVersion.v30;
+                    }
+                    else if (sVersion.Equals("Unknown", StringComparison.OrdinalIgnoreCase))
+                    {
+                        version = Request.ProtocolVersion.Unknown;
+                    }
+                    else
+                    {
+                        customVersion = sVersion;
+                    }
+                }
+
+                Console.Write("\nVersion Policy: ");
+                string? pVersion = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(pVersion))
+                {
+                    if (pVersion.Equals("RequestVersionExact", StringComparison.OrdinalIgnoreCase) || pVersion.Equals("Exact", StringComparison.OrdinalIgnoreCase))
+                    {
+                        policy = HttpVersionPolicy.RequestVersionExact;
+                    }
+                    else if (pVersion.Equals("RequestVersionOrHigher", StringComparison.OrdinalIgnoreCase) || pVersion.Equals("Higher", StringComparison.OrdinalIgnoreCase))
+                    {
+                        policy = HttpVersionPolicy.RequestVersionOrHigher;
+                    }
+                    else if (pVersion.Equals("RequestVersionOrLower", StringComparison.OrdinalIgnoreCase) || pVersion.Equals("Lower", StringComparison.OrdinalIgnoreCase))
+                    {
+                        policy = HttpVersionPolicy.RequestVersionOrLower;
+                    }
+                }
+
+                Console.Write("\nUse Proxy (Default is No)? ");
+                string? proxyConfS = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(proxyConfS) && (proxyConfS.Equals("y", StringComparison.OrdinalIgnoreCase) || proxyConfS.Equals("yes", StringComparison.OrdinalIgnoreCase) || proxyConfS.Equals("true", StringComparison.OrdinalIgnoreCase)))
+                {
+                    proxy = new string[3];
+                    Console.Write("\n- Proxy URI: ");
+                    string? proxyURI = Console.ReadLine();
+                    if (!string.IsNullOrEmpty(proxyURI))
+                    {
+                        proxy[0] = proxyURI;
+                    }
+                    Console.Write("\n- Use Credentials? ");
+                    string? credentialsConfS = Console.ReadLine();
+                    if (!string.IsNullOrWhiteSpace(credentialsConfS) && (credentialsConfS.Equals("y", StringComparison.OrdinalIgnoreCase) || credentialsConfS.Equals("yes", StringComparison.OrdinalIgnoreCase) || credentialsConfS.Equals("true", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        Console.Write("\n- Username: ");
+                        string? proxyUsername = Console.ReadLine();
+                        Console.Write("\n- Password: ");
+                        string? proxyPassword = Console.ReadLine();
+                        if (!string.IsNullOrEmpty(proxyUsername) && !string.IsNullOrEmpty(proxyPassword))
+                        {
+                            proxy[1] = proxyUsername;
+                            proxy[2] = proxyPassword;
+                        }
+                    }
+                }
+
+                Console.Write("\nAllow AutoRedirects (Default is True)? ");
+                string? redirectConfS = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(redirectConfS) && (redirectConfS.Equals("n", StringComparison.OrdinalIgnoreCase) || redirectConfS.Equals("no", StringComparison.OrdinalIgnoreCase) || redirectConfS.Equals("false", StringComparison.OrdinalIgnoreCase)))
+                {
+                    autoRedirects = false;
+                }
+
+                Console.Write("\nUse SSL Verification (Default is False)? ");
+                string? SSLConfS = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(SSLConfS) && (SSLConfS.Equals("y", StringComparison.OrdinalIgnoreCase) || SSLConfS.Equals("yes", StringComparison.OrdinalIgnoreCase) || SSLConfS.Equals("true", StringComparison.OrdinalIgnoreCase)))
+                {
+                    ssl = true;
+                }
+
+                Console.Write("\nSSL Protocol: ");
+                string? SSLp = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(SSLp))
+                {
+                    if (SSLp.Equals("n", StringComparison.OrdinalIgnoreCase) || SSLp.Equals("no", StringComparison.OrdinalIgnoreCase) || SSLp.Equals("none", StringComparison.OrdinalIgnoreCase))
+                    {
+                        SSLprotocol = SslProtocols.None;
+                    }
+                    else if (SSLp.Equals("d", StringComparison.OrdinalIgnoreCase) || SSLp.Equals("default", StringComparison.OrdinalIgnoreCase))
+                    {
+                        SSLprotocol = SslProtocols.Default;
+                    }
+                    else if (SSLp.Equals("SSL2", StringComparison.OrdinalIgnoreCase) || SSLp.Equals("SSL 2", StringComparison.OrdinalIgnoreCase))
+                    {
+                        SSLprotocol = SslProtocols.Ssl2;
+                    }
+                    else if (SSLp.Equals("SSL3", StringComparison.OrdinalIgnoreCase) || SSLp.Equals("SSL 3", StringComparison.OrdinalIgnoreCase))
+                    {
+                        SSLprotocol = SslProtocols.Ssl3;
+                    }
+                    else if (SSLp.Equals("TLS", StringComparison.OrdinalIgnoreCase))
+                    {
+                        SSLprotocol = SslProtocols.Tls;
+                    }
+                    else if (SSLp.Equals("TLS11", StringComparison.OrdinalIgnoreCase) || SSLp.Equals("TLS1.1", StringComparison.OrdinalIgnoreCase) || SSLp.Equals("TLS 11", StringComparison.OrdinalIgnoreCase) || SSLp.Equals("TLS 1.1", StringComparison.OrdinalIgnoreCase))
+                    {
+                        SSLprotocol = SslProtocols.Tls11;
+                    }
+                    else if (SSLp.Equals("TLS12", StringComparison.OrdinalIgnoreCase) || SSLp.Equals("TLS1.2", StringComparison.OrdinalIgnoreCase) || SSLp.Equals("TLS 12", StringComparison.OrdinalIgnoreCase) || SSLp.Equals("TLS 1.2", StringComparison.OrdinalIgnoreCase))
+                    {
+                        SSLprotocol = SslProtocols.Tls12;
+                    }
+                    else if (SSLp.Equals("TLS13", StringComparison.OrdinalIgnoreCase) || SSLp.Equals("TLS1.3", StringComparison.OrdinalIgnoreCase) || SSLp.Equals("TLS 13", StringComparison.OrdinalIgnoreCase) || SSLp.Equals("TLS 1.3", StringComparison.OrdinalIgnoreCase))
+                    {
+                        SSLprotocol = SslProtocols.Tls13;
+                    }
+                }
+
+                Console.Write("\nDecompression Method: ");
+                string? decP = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(decP))
+                {
+                    if (decP.Equals("a", StringComparison.OrdinalIgnoreCase) || decP.Equals("all", StringComparison.OrdinalIgnoreCase) || decP.Equals("everything", StringComparison.OrdinalIgnoreCase))
+                    {
+                        decompression = DecompressionMethods.All;
+                    }
+                    else if (decP.Equals("b", StringComparison.OrdinalIgnoreCase) || decP.Equals("brotli", StringComparison.OrdinalIgnoreCase))
+                    {
+                        decompression = DecompressionMethods.Brotli;
+                    }
+                    else if (decP.Equals("d", StringComparison.OrdinalIgnoreCase) || decP.Equals("deflate", StringComparison.OrdinalIgnoreCase))
+                    {
+                        decompression = DecompressionMethods.Deflate;
+                    }
+                    else if (decP.Equals("g", StringComparison.OrdinalIgnoreCase) || decP.Equals("gzip", StringComparison.OrdinalIgnoreCase))
+                    {
+                        decompression = DecompressionMethods.GZip;
+                    }
+                    else if (decP.Equals("n", StringComparison.OrdinalIgnoreCase) || decP.Equals("no", StringComparison.OrdinalIgnoreCase) || decP.Equals("none", StringComparison.OrdinalIgnoreCase))
+                    {
+                        decompression = DecompressionMethods.None;
+                    }
+                }
+
+
+                Console.Clear();
+                HttpResponseMessage? response = Request.Create(URI, method, headers, body, cookies, version, customVersion, policy, proxy, autoRedirects, ssl, SSLprotocol, decompression).GetAwaiter().GetResult();
+
+                if (response != null)
+                {
+                    Dictionary<string, string> heads = response.Headers.ToDictionary(a => a.Key, a => string.Join(";", a.Value));
+                    Dictionary<string, string> trailingHeaders = response.TrailingHeaders.ToDictionary(a => a.Key, a => string.Join(";", a.Value));
+
+                    Console.WriteLine("RESPONSE:\n-----------------------------\n");
+                    Console.WriteLine("Status Code: " + response.StatusCode);
+                    if (!string.IsNullOrWhiteSpace(response.ReasonPhrase)) { Console.WriteLine("Reason Phrase: " + response.ReasonPhrase); }
+                    Console.WriteLine($"Protocol Version: {protocol} -> {response.Version}");
+                    Console.WriteLine("Headers:");
+                    for (int i = 0; i < heads.Count; i++)
+                    {
+                        Console.WriteLine($"- {heads.ElementAt(i).Key}: {heads.ElementAt(i).Value}");
+                    }
+                    Console.WriteLine("Trailing Headers:");
+                    for (int i = 0; i < trailingHeaders.Count; i++)
+                    {
+                        Console.WriteLine($"- {trailingHeaders.ElementAt(i).Key}: {trailingHeaders.ElementAt(i).Value}");
+                    }
+                    Console.Write("\nBody:\n" + response.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+                }
+
+                Console.ReadKey();
+            }
+        }
+
+
+        private static void Initialize()
+        {
+            URI = null;
+            method = null;
+            body = null;
+            customVersion = null;
+            protocol = "1.1";
+            proxy = null;
+            autoRedirects = true;
+            ssl = false;
+            headers = null;
+            cookies = null;
+            version = null;
+            policy = null;
+            SSLprotocol = null;
+            decompression = null;
+        }
+    }
+}
---
+++ Request.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Net.Http;
+using System.ComponentModel;
+using System.Security.Authentication;
+
+namespace RequestFaker
+{
+    internal class Request
+    {
+        public static async Task<HttpResponseMessage?> Create(string? URI, string? method, Dictionary<string, string>? headers = null, string? body = null, Dictionary<string, string>? cookies = null, ProtocolVersion? version = null, string? customVersion = null, HttpVersionPolicy? policy = null, string[]? proxy = null, bool autoRedirects = true, bool ssl = false, SslProtocols? SSLProtocol = null, DecompressionMethods? decompression = null)
+        {
+            HttpClient client;
+            HttpResponseMessage response = new();
+            HttpClientHandler handler = new();
+            CookieContainer container = new();
+            if (string.IsNullOrWhiteSpace(URI) || string.IsNullOrWhiteSpace(method)) { return new(); }
+
+            // FORGING REQUEST
+            using (HttpRequestMessage request = new(new(method), URI))
+            {
+                // HEADERS
+                if (headers != null)
+                {
+                    for (int i = 0; i < headers.Count; i++)
+                    {
+                        request.Headers.Add(headers.Keys.ToList()[i], headers.Values.ToList()[i]);
+                    }
+                }
+
+                // CONTENT
+                if (body != null) { request.Content = new ByteArrayContent(Encoding.UTF8.GetBytes(body)); }
+
+                // VERSION
+                if (version != null)
+                {
+                    switch (version)
+                    {
+                        case ProtocolVersion.v10:
+                            request.Version = HttpVersion.Version10;
+                            Program.Protocol = "1.0";
+                            break;
+                        case ProtocolVersion.v11:
+                            request.Version = HttpVersion.Version11;
+                            break;
+                        case ProtocolVersion.v20:
+                            request.Version = HttpVersion.Version20;
+                            Program.Protocol = "2.0";
+                            break;
+                        case ProtocolVersion.v30:
+                            request.Version = HttpVersion.Version30;
+                            Program.Protocol = "3.0";
+                            break;
+                        case ProtocolVersion.Unknown:
+                            request.Version = HttpVersion.Unknown;
+                            Program.Protocol = "Unknown";
+                            break;
+                    }
+                }
+                else if (!string.IsNullOrWhiteSpace(customVersion))
+                {
+                    request.Version = new(customVersion);
+                    Program.Protocol = customVersion;
+                }
+
+                // VERSION POLICY
+                if (policy != null) { request.VersionPolicy = (HttpVersionPolicy)policy; }
+
+                // COOKIES
+                if (cookies != null)
+                {
+                    handler.UseCookies = true;
+                    for (int i = 0; i < cookies.Count; i++)
+                    {
+                        container.Add(new Uri(URI), new Cookie(cookies.ElementAt(i).Key, cookies.ElementAt(i).Value));
+                    }
+                }
+                else
+                {
+                    handler.UseCookies = false;
+                }
+
+                // PROXY
+                if (proxy != null)
+                {
+                    handler.UseProxy = true;
+
+                    WebProxy webProxy = new()
+                    {
+                        BypassProxyOnLocal = false,
+                        UseDefaultCredentials = false
+                    };
+                    if (proxy[0] != null)
+                    {
+                        webProxy.Address = new(proxy[0]);
+                    }
+                    if (proxy[1] != null && proxy[2] != null)
+                    {
+                        webProxy.Credentials = new NetworkCredential(proxy[1], proxy[2]);
+                    }
+                    handler.Proxy = webProxy;
+                }
+
+                // SSL SETTINGS
+                if (!ssl) { handler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator; }
+                if (SSLProtocol != null) { handler.SslProtocols = (SslProtocols)SSLProtocol; }
+
+                // FINAL SETTINGS
+                handler.AllowAutoRedirect = autoRedirects;
+                if (decompression != null) { handler.AutomaticDecompression = (DecompressionMethods)decompression; }
+
+                // SEND
+                handler.CookieContainer = container;
+                client = new(handler, true);
+                try
+                {
+                    response = await client.SendAsync(request);
+                }
+                catch (Win32Exception e)
+                {
+                    Console.Write(e.Message);
+                    return null;
+                }
+                catch (Exception e)
+                {
+                    Console.Write(e.Message);
+                    return null;
+                }
+            }
+            return response;
+        }
+
+
+        public enum ProtocolVersion
+        {
+            v10, v11, v20, v30, Unknown
+        };
+    }
+}
